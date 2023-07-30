3a49c4c7b7b89e044f766ed14dbabf5dfeff6e42
Ericsen Tsai <bommer95175@gmail.com>
2023-07-30 15:01:58 +0800

    feat: add canvas draw with ws

--- src/components/PainCanvas.tsx
+++ src/components/PainCanvas.tsx
@@ -1,10 +1,39 @@
 'use client'
 import { useRef, useState, useEffect } from 'react'
+import { io } from 'socket.io-client'
+import type { Socket } from 'socket.io-client'
+
+import type { ClientToServerEvents } from '@/libs/wss/events/client-to-server'
+import type { ServerToClientEvents } from '@/libs/wss/events/server-to-client'
+import { env } from '@/env.mjs'
+
+let socket: Socket<ServerToClientEvents, ClientToServerEvents>
+
+async function getSocketConnection() {
+  socket = io(env.NEXT_PUBLIC_WSS_URL) // KEEP AS IS
+}
 
 function PainCanvas() {
+  const userIdRef = useRef<string>(Math.random().toString())
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const contextRef = useRef<CanvasRenderingContext2D | null>(null)
   const [isDrawing, setIsDrawing] = useState(false)
+
+  const getCurrentCanvasData = () => {
+    const canvas = canvasRef.current
+    if (!canvas) return
+    const data = canvas.toDataURL()
+    return data
+  }
+
+  const loadCanvasData = (data: string) => {
+    const img = new Image()
+    img.onload = () => {
+      const context = canvasRef.current?.getContext('2d')
+      context?.drawImage(img, 0, 0)
+    }
+    img.src = data
+  }
 
   useEffect(() => {
     const canvas = canvasRef.current
@@ -19,6 +48,24 @@ function PainCanvas() {
     context.strokeStyle = 'black'
     context.lineWidth = 5
     contextRef.current = context
+  }, [])
+
+  useEffect(() => {
+    getSocketConnection().then(() => {
+      socket.on('connect', () => {
+        socket.on('CanvasResponse', async (payload) => {
+          const { dataURL } = payload
+          loadCanvasData(dataURL)
+        })
+        socket.emit('CanvasJoin', {
+          userId: userIdRef.current,
+        })
+      })
+    })
+    return () => {
+      socket.disconnect()
+      socket.off('connect')
+    }
   }, [])
 
   const startDrawing = ({ nativeEvent }: { nativeEvent: MouseEvent }) => {
@@ -40,25 +87,24 @@ function PainCanvas() {
     const { offsetX, offsetY } = nativeEvent
     contextRef.current?.lineTo(offsetX, offsetY)
     contextRef.current?.stroke()
+
+    const data = getCurrentCanvasData()
+    if (data) {
+      socket.emit('CanvasUpdate', { dataURL: data, userId: userIdRef.current })
+    }
   }
 
-  const saveDrawing = () => {
-    const canvas = canvasRef.current
-    if (!canvas) return
-    const data = canvas.toDataURL()
-    localStorage.setItem('drawing', data)
+  const saveDrawingToStorage = () => {
+    const data = getCurrentCanvasData()
+    if (data) {
+      localStorage.setItem('drawing', data)
+    }
   }
 
-  const loadDrawing = () => {
+  const loadDrawingFromStorage = () => {
     const data = localStorage.getItem('drawing')
     if (!data) return
-
-    const img = new Image()
-    img.onload = () => {
-      const context = canvasRef.current?.getContext('2d')
-      context?.drawImage(img, 0, 0)
-    }
-    img.src = data
+    loadCanvasData(data)
   }
 
   return (
@@ -73,13 +119,13 @@ function PainCanvas() {
       <div className="flex gap-5 absolute top-5 right-5">
         <button
           className="text-lg font-bold p-5 ring-2 ring-pink-500 rounded-lg bg-white"
-          onClick={saveDrawing}
+          onClick={saveDrawingToStorage}
         >
           SAVE !!
         </button>
         <button
           className="text-lg font-bold p-5 ring-2 ring-pink-500 rounded-lg bg-white"
-          onClick={loadDrawing}
+          onClick={loadDrawingFromStorage}
         >
           LOAD !!
         </button>
---
+++ src/env.mjs
@@ -0,0 +1,20 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  /*
+   * Specify what prefix the client-side variables must have.
+   * This is enforced both on type-level and at runtime.
+   */
+  server: {},
+  client: {
+    NEXT_PUBLIC_WSS_URL: z.string().min(1),
+  },
+  /**
+   * What object holds the environment variables at runtime.
+   * Often `process.env` or `import.meta.env`
+   */
+  runtimeEnv: {
+    NEXT_PUBLIC_WSS_URL: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+  },
+})
